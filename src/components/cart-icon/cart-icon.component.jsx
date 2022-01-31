dfb06293e5be2e76095048d669b51ddc9b15c734
Jacques <vanenkie@gmail.com>
2022-01-31 11:17:59 +0200

    updating the appliction with reselect, replaced cart selectors with memoized selectors.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,15 +3,16 @@ import React from "react";
 import { connect} from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) =>  (
-    <div className="cart-icon" onClick={toggleCartHidden}>
+const CartIcon = ({toggleCartHiddenFlag, itemCount}) =>  (
+    <div className="cart-icon" onClick={toggleCartHiddenFlag}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
         
     </div>
 )
@@ -19,8 +20,16 @@ const CartIcon = ({toggleCartHidden}) =>  (
 const mapDispatchToProps = dispatch => 
 (
     {
-        toggleCartHidden: () => dispatch(toggleCartHidden())
+        toggleCartHiddenFlag: () => dispatch(toggleCartHidden())
     }
 )
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+//We replace the above with cart.selector component in redux folder
+const mapStatetoProps = state =>
+(
+    {
+        itemCount: selectCartItemsCount(state)
+    }
+)
+
+export default connect(mapStatetoProps,mapDispatchToProps)(CartIcon);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,33 @@
+// We are going to add reusable redux components here for cart
+import  { createSelector } from 'reselect';
+
+//Input selector
+//select part of the state (just the cart state)
+const selectCart = state => state.cart;
+
+//createSelector(array,callBackFunction)
+//This is now considered a memoized selector
+// Memoization is remebering the result of a calculation with th same paramaters so it does not need to be done again (caching result with specific paramters)
+//createSelector([inputSlectors], fucntion to perform)
+export const selectCartItems= createSelector(
+    [selectCart], 
+    cart => cart.cartItems 
+);
+
+// reduce() is an array method
+// the result of reduce is one value
+// reduce((accumulator, currentrElement) => the_operatation we want to perform ), initial value)
+// This will iterate through the array staring at element 0 and pass the elment as cuurentElement plus the accumulatyed value which would start as 0 as per the default
+// each time the process iterates it will re-render the component which in effect updates the item-count above
+// itemCount: cartItems.reduce((accumulatedQuanity, cartItem) => accumulatedQuanity + cartItem.quantity, 0)
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => 
+       cartItems.reduce(
+           (accumulatedQuanity, cartItem) => 
+               accumulatedQuanity + cartItem.quantity, 
+               0
+        )
+
+);
+
