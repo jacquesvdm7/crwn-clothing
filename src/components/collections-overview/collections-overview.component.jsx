1d70a96a22461cc56d2d4d3695d2643ced1b4456
Jacques <vanenkie@gmail.com>
2022-02-03 09:58:43 +0200

    Normalise shop.data.js

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import PreviewCollections from '../preview-collection/preview-collection.component';
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 
 import './collections-overview.styles.scss';
 
@@ -20,7 +20,7 @@ const CollectionsOverview =({collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -3,19 +3,26 @@ import { createSelector } from 'reselect';
 //The property state.shop coms from the name used in the rootReducer
 const selectShop = state => state.shop;
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
+// const COLLECTION_ID_MAP = {
+//     hats: 1,
+//     sneakers: 2,
+//     jackets: 3,
+//     womens: 4,
+//     mens: 5
 
-}
+// }
 
 export const selectCollections = createSelector(
     [selectShop], 
     shop => shop.collections
 );
+
+//We changed shop.data.ts to object so now we need to convert it to an array for collectionPrevie page
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections], 
+    //We need to get all the keys of the hashtable first and then take the result and use map to get every key lement and the get the collections at that key value
+    collections => Object.keys(collections).map(key => collections[key])
+)
 
 // find collection.id from matching url paramter of our collction map defined as COLLECTION_ID_MAP
 // the array that is passed is the result of funciont "selectCollections" define above
@@ -24,6 +31,9 @@ export const selectCollection = collectionUrlParam =>
 // createSelector is a function that restruns another function
 createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    // When this array gets very big it will call the function on every element to compare which can become very time consuming with big arrays
+    // We need to nomrlaise the data, we will change our shop.data.js to an object instead of an array, we replace the array find as comment out below
+    // collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    collections => collections[collectionUrlParam]
 
 )
